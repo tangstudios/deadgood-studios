4daa0d8f41f5c27f306aaf8bd79258cd875ba097
Chris Tang <tangch3@hotmail.com>
2025-02-05 22:39:01 +0000

    add logo

--- nextjs-deadgood-studios/src/app/footer/page.tsx
+++ nextjs-deadgood-studios/src/app/footer/page.tsx
@@ -1,4 +1,5 @@
 import AnimateText from "@/components/animate-text/AnimateText";
+import Image from "next/image";
 
 const footerHeight = 500;
 
@@ -20,17 +21,20 @@ export default function Footer() {
             height: `${footerHeight}px`,
             top: `calc(100vh - ${footerHeight}px)`,
           }}
-          className="sticky flex flex-col justify-center text-[10vmin]"
+          className="sticky justify-between flex flex-row items-center "
         >
-          <AnimateText text="Hey! Get in touch?" rotateOnHover />
-          <AnimateText text="Contact" rotateOnHover />
-          <a
-            className="hover:text-gray-400"
-            href="mailto:hi@deadgood.studio"
-            target="_blank"
-          >
-            <AnimateText text="hi@deadgood.studio" />
-          </a>
+          <div className="flex flex-col  text-[10vmin]">
+            <AnimateText text="Hey! Get in touch?" rotateOnHover />
+            <AnimateText text="Contact" rotateOnHover />
+            <a
+              className="hover:text-gray-400"
+              href="mailto:hi@deadgood.studio"
+              target="_blank"
+            >
+              <AnimateText text="hi@deadgood.studio" />
+            </a>
+          </div>
+          <Image src="/logo.jpg" alt="logo" height={600} width={600} />
         </div>
       </div>
     </div>
