5b2363f6862d6e58a866c0afde13e284f647de85
Chris Tang <tangch3@hotmail.com>
2025-09-15 13:37:55 +0800

    font changes

--- nextjs-deadgood-studios/src/components/Sidebar/Sidebar.tsx
+++ nextjs-deadgood-studios/src/components/Sidebar/Sidebar.tsx
@@ -4,30 +4,23 @@ import Link from "next/link";
 export default function Sidebar() {
   return (
     <aside className="w-[28%] max-h-screen sticky top-0 overflow-y-auto scrollbar-hide p-8">
-      <div className="flex flex-col gap-2">
-        <Image
-          src="/logo.jpg"
-          alt="logo"
-          width={80}
-          height={80}
-          className="z-10"
-          priority
-        />
-        <nav className="pt-10 space-y-2">
-          <Link className="block text-5xl uppercase" href="/work">
-            Work
+      <div className="flex flex-col gap-12">
+        <Image src="/logo.jpg" alt="logo" width={100} height={100} priority />
+        <nav className="flex flex-col gap-4">
+          <Link href="/work">
+            <h2 className="block text-5xl uppercase">Work</h2>
           </Link>
-          <Link className="block text-5xl uppercase" href="/about">
-            About
+          <Link href="/about">
+            <h2 className="block text-5xl uppercase">About</h2>
           </Link>
-          <Link className="block text-5xl uppercase" href="/dinkum">
-            Dinkum
+          <Link href="/dinkum">
+            <h2 className="block text-5xl uppercase">Dinkum</h2>
           </Link>
         </nav>
-        <p className="text-xl font-thin pt-6">
+        <h1 className="text-xl font-thin">
           Dead Good serves as the vital connection between our clients and their
           target audience, bridging the gap through effective communication.
-        </p>
+        </h1>
       </div>
     </aside>
   );
