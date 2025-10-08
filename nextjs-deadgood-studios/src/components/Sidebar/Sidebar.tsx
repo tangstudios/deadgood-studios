f9965ddca627e7c878fad2f4e23e1f386a461b31
Chris Tang <tangch3@hotmail.com>
2025-10-08 11:46:28 +0800

    dynamic menu item text sizing

--- nextjs-deadgood-studios/src/components/Sidebar/Sidebar.tsx
+++ nextjs-deadgood-studios/src/components/Sidebar/Sidebar.tsx
@@ -17,25 +17,25 @@ export default function Sidebar() {
         </Link>
         {/* TOP: scrollable area only */}
         <div className="flex-1 overflow-y-auto scrollbar-hide">
-          <div className="pt-8 flex flex-col gap-6">
-            <nav className="flex flex-col gap-4">
+          <div className="pt-8 flex flex-col gap-8">
+            <nav className="flex flex-col">
               <Link href="/work">
-                <h2 className="block text-6xl uppercase transition-colors duration-500 hover:text-neutral-600">
+                <h2 className="text-[clamp(2rem,3.5vw,6rem)] uppercase transition-colors duration-500 hover:text-neutral-600">
                   Work
                 </h2>
               </Link>
               <Link href="/about">
-                <h2 className="block text-6xl uppercase transition-colors duration-500 hover:text-neutral-600">
+                <h2 className="text-[clamp(2rem,3.5vw,6rem)] uppercase transition-colors duration-500 hover:text-neutral-600">
                   About
                 </h2>
               </Link>
               <Link href="/dinkum">
-                <h2 className="block text-6xl uppercase transition-colors duration-500 hover:text-neutral-600 ">
+                <h2 className="text-[clamp(2rem,3.5vw,6rem)] uppercase transition-colors duration-500 hover:text-neutral-600 ">
                   Dinkum
                 </h2>
               </Link>
             </nav>
-            <h1 className="text-xl font-thin">
+            <h1 className="text-[clamp(1rem,2vw,1.25rem)] font-thin pr-8">
               Dead Good serves as the vital connection between our clients and
               their target audience, bridging the gap through effective
               communication.
@@ -54,7 +54,7 @@ export default function Sidebar() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h2 className="block text-4xl uppercase transition-colors duration-500 hover:text-neutral-600">
+              <h2 className="block text-[clamp(1.5rem,2vw,6rem)] uppercase transition-colors duration-500 hover:text-neutral-600">
                 Contact
               </h2>
             </Link>
@@ -63,7 +63,7 @@ export default function Sidebar() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h2 className="block text-4xl uppercase transition-colors duration-500 hover:text-neutral-600">
+              <h2 className="block text-[clamp(1.5rem,2vw,6rem)] uppercase transition-colors duration-500 hover:text-neutral-600">
                 Instagram
               </h2>
             </Link>
@@ -72,7 +72,7 @@ export default function Sidebar() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h2 className="block text-4xl uppercase transition-colors duration-500 hover:text-neutral-600">
+              <h2 className="block text-[clamp(1.5rem,2vw,6rem)] uppercase transition-colors duration-500 hover:text-neutral-600">
                 Vimeo
               </h2>
             </Link>
