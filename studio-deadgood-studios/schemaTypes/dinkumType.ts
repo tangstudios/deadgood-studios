772daccd56242759f7d039d0e9947fb36dd03515
Chris Tang <tangch3@hotmail.com>
2025-10-08 13:14:48 +0800

    add new dinkum type

---
+++ studio-deadgood-studios/schemaTypes/dinkumType.ts
@@ -0,0 +1,43 @@
+import {defineField, defineType} from 'sanity'
+
+export const dinkumType = defineType({
+  name: 'teamMembers',
+  title: 'Team Members',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'episode',
+      type: 'number',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'podcastTrailerOrThumbnail',
+      title: 'Podcast Trailer / Thumbnail',
+      type: 'array',
+      of: [
+        {type: 'image', options: {hotspot: true}},
+        {type: 'file', title: 'Video (MP4)', options: {accept: 'video/mp4'}},
+      ],
+      validation: (r) => r.required().min(1).max(1),
+      description: 'Choose either a single image or a single MP4 video.',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+      description: 'Short podcast episode summary',
+      validation: (rule) => rule.required(),
+    }),
+  ],
+})
--- studio-deadgood-studios/schemaTypes/index.ts
+++ studio-deadgood-studios/schemaTypes/index.ts
@@ -1,5 +1,6 @@
+import {dinkumType} from './dinkumType'
 import {projectType} from './projectType'
 import {tagType} from './tagType'
 import {teamMemberType} from './teamMemberType'
 
-export const schemaTypes = [projectType, tagType, teamMemberType]
+export const schemaTypes = [projectType, tagType, teamMemberType, dinkumType]
